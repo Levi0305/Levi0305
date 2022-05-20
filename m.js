071f5797dcc4834ef8088a351a6e3d03603ae67b
Levi0305 <105902026+Levi0305@users.noreply.github.com>
2022-05-19 23:22:32 -0500

    Add files via upload

---
+++ m.js
@@ -0,0 +1,33 @@
+alert("Hola espero y estes bien.");
+alert("Aqui van unas frases y palabras que me gustarian decirte pero me da cosita, o como dice la chaviza me da pene.");
+alert("Tqm");
+alert("Me haces feliz nuca me dejes:(");
+alert("Desde que llegaste a mi vida todo a sido felicidad.");
+alert("Se que no siempre nos vemos, pero cuando estoy contigo siento que estoy en mi lugar feliz<3");
+alert("Tu rostro es como ver el cielo, ver tus ojos es ver el universo, tocar tus manos es sentirse en el cielo y estar en el paraiso.");
+alert("Te quiero como para escuchar tu risa toda la noche y dormir en tu pecho, sin sombras ni fantasmas... te quiero como para no soltarte jamás.         -Mario Benedetti");
+alert("No se si fue obra del destino o fue pura coincidencia conocerte pero le doy la gracias a la vida por haber coincidido contigo, gracias a ello conoci a la persona más bonita que hay en esta galaxia <3");
+alert("Feliz, por que existes tú <3");
+alert("La mayor declaración de amor es la que no se hace; el hombre que siente mucho habla.     -Platón");
+alert("Me encanta la forma tan bonita que tienes de sonreír e iluminar mi mundo 🌍✨");
+alert("Todavia me pongo nerviso cuando sé que te voy a ver <3");
+alert("Tengo en mente que nada es para siempre, pero a tí te quiero tener hasta donde se pueda o hasta donde tú quieras estar conmigo <3");
+alert("Te amaré en todos lados, en todos los universos y en todas las vidas.   Siempre encotraré el camino para volver a ti sin importar qué pase, lo prometo.");
+alert("Por mas triste, por mas roto que yo este siempre pondré mi mejor cara para apoyarte cuando tu estes mal y ni siquiera notes que estoy muriendo. Todo con tal de poderte hacerte lo mas feliz en tus momentos difíciles, para poder ver siempre tu hermosa sonrisa");
+alert("Me tienes a mi sabes? No soy la gran cosa pero... Me tienes a mi <3");
+alert("Aún recuerdo el día que llegaste a mi vida sin avisar, sabía que esa sonrisa no la iba a olvidar, porque sabía que de ti me iba a enamorar.");
+alert("A mi si me gusta q seas empalagosa y súper cariñosa en exceso.");
+alert("¿Aburrirme tú? jsjsjsjs, no tenía ganas de nada, me enojaba por todo y mis días eran aburridos, pero desde que llegaste me siento muy feliz<3");
+alert("Me da igual su cuerpo, ¿viste su sonrisa?, su forma de ser, de pensar, el sonido de su voz, como trata a las personas, ella simplemente es perfecta. <3");
+alert("Realmente siento algo bonito por ti :')");
+alert("Sabes no importa cuánto tiempo pase, cuántos rostros vea, cuántos ojos me mire, cuántas estaciones tenga que pasar, mi corazon siempre sera tuyo hasta el ultimo latido <3");
+alert("Gracias por ser ese rayito de luz que ilumina mi vida");
+alert("Gracias por preocuparte por mi");
+alert("Gracias por las sonrisas");
+alert("Gracias por enseñarme ser más fuerte");
+alert("Gracias por estar orgullosa de mi");
+alert("Gracias por escucharme y ser una persona increible");
+alert("Simplemente, Gracias por todo.... Te amo<3");
+
+
+
